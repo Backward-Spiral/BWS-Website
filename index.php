64312515246a551351054a955bb82aa338aa7659
Maxwell Reo Ramsdell <S133009096@besd.net>
2017-05-03 11:39:41 -0600

    Implementing Object Oriented CSS

--- index.php
+++ index.php
@@ -7,14 +7,14 @@
         <link rel="stylesheet" type="text/css" href="CSS/main.css" />
     </head>
     <body>
-        <div class="headbar"> 
+        <div class="headbar background"> 
             <img src="Pics/TransparentSpiral.png" class="logoPic"/>
-            <p class="logo">Backwards Spiral</p>
+            <p class="logo menuFont">Backwards Spiral</p>
             <ul>
-                <li class="menuOption" id="4">contact();</li>
-                <li class="menuOption" id="3">about;</li>
-                <li class="menuOption" id="2">"games"</li>
-                <li class="menuOption" id="1">Home:</li>
+                <li class="menuOption menuFont" id="4">contact();</li>
+                <li class="menuOption menuFont" id="3">about;</li>
+                <li class="menuOption menuFont" id="2">"games"</li>
+                <li class="menuOption menuFont" id="1">Home:</li>
             </ul>
         </div>
     </body>
