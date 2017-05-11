bd7a000ef49ecc108d63ead2f509ab37cf762658
Max <madramsdell@gmail.com>
2017-05-10 22:57:20 -0600

    Finally have everything up to speed as far as structure goes.

--- index.php
+++ index.php
@@ -11,8 +11,10 @@
 
         <!-- Head bar markup. Is fixed with logo and a few links on the bar. -->
         <div class="headbar highlight_black"> 
-            <img src="Pics/TransparentSpiral.png" class="logoPic"/>
-            <p class="logo logoFont">Backwards Spiral</p>
+            <div class="logoContainer">
+                <img src="Pics/TransparentSpiral.png" class="logoPic"/>
+                <p class="logo logoFont">Backwards Spiral</p>
+            </div>
             <ul class="menuContainer"> <!-- Menu markup -->
                 <li class="menuOption headerFont highlightHeader" id="1">home:</li>
                 <li class="menuOption headerFont" id="2">"games"</li>
@@ -23,7 +25,14 @@
 
         <!-- Test code for an article body -->
         <div class="whitespace"> </div>
-        <div class="article"> </div>
+        <div class="article articleSkin">
+            <div class="articleHeader">
+                
+            </div> 
+            <div class="articleBody">
+
+            </div>
+        </div>
         
     </body>
 </html>
